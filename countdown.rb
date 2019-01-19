1ba530720e78db1784ef4fedd59dd1dea9233c0e
rtboudreau <rtboudreau@gmail.com>
2019-01-18 20:11:28 -0800

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(x)
+	x = 10
+	while x > 0 
+		puts "#{x} SECOND(S)!"
+		x -= 1
+	end
+	return "Happy New Year!".upcase
+end
+
+
+def countdown_with_sleep(x)
+	x = 10
+	while x > 0 
+		puts "#{x} SECOND(S)!"
+		x -= 1
+		sleep(0.5)
+	end
 end
